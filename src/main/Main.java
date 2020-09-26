f8b760e8165305e072a6c368a7edf97f30a572dd
Leonard <thongyixuen@gmail.com>
2020-09-26 16:06:17 -0400

    fix bugs on Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,6 +14,15 @@ public class Main extends Application {
     private static Stage stage = new Stage();
     private static Player player = new Player();
 
+    @Override
+    public void start(Stage primaryStage) {
+        stage = primaryStage;
+        stage.setTitle("FarmSim");
+        stage.setScene(WelcomeScreen.getScene());
+        stage.setMaximized(true);
+        stage.show();
+    }
+
     public static Stage getStage() {
         return stage;
     }
@@ -24,13 +33,5 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("FarmSim");
-        primaryStage.setScene(WelcomeScreen.getScene());
-        primaryStage.setMaximized(true);
-        primaryStage.show();
     }
 }
