91c9fad6c123f22da13f3a4f0f8bf1347c6ecd23
Leonard <thongyixuen@gmail.com>
2020-09-26 04:15:24 -0400

    refactor ConfigScene file and add basic styling

--- src/scenes/ConfigScene.java
+++ src/scenes/ConfigScene.java
@@ -4,13 +4,16 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import main.Main;
 
 
@@ -21,36 +24,20 @@ public class ConfigScene {
         Label nameLabel = new Label("Username:");
         TextField nameTextField = new TextField();
 
-        HBox nameHBox = new HBox();
-        nameHBox.getChildren().addAll(nameLabel, nameTextField);
-        nameHBox.setSpacing(10);
-
         // Label Drop Down for Difficulty
         Label diffLabel = new Label("Select a difficulty:");
         ObservableList<String> diffs = FXCollections.observableArrayList("Easy", "Normal", "Hard");
         ComboBox<String> diffComboBox = new ComboBox<>(diffs);
-
-        HBox diffHBox = new HBox();
-        diffHBox.getChildren().addAll(diffLabel, diffComboBox);
-        diffHBox.setSpacing(10);
 
         // Label and Drop Down for Starting Seed
         Label seedLabel = new Label("Select a starting seed:");
         ObservableList<String> seeds = FXCollections.observableArrayList("Corn", "Sunflower", "Cauliflower");
         ComboBox<String> seedComboBox = new ComboBox<>(seeds);
 
-        HBox seedHBox = new HBox();
-        seedHBox.getChildren().addAll(seedLabel, seedComboBox);
-        seedHBox.setSpacing(10);
-
         // Label and Drop Down for Starting Season
         Label seasonLabel = new Label("Select a starting season:");
         ObservableList<String> seasons = FXCollections.observableArrayList("Spring", "Summer", "Fall", "Winter");
         ComboBox<String> seasonComboBox = new ComboBox<>(seasons);
-
-        HBox seasonHBox = new HBox();
-        seasonHBox.getChildren().addAll(seasonLabel, seasonComboBox);
-        seasonHBox.setSpacing(10);
 
         // Button to continue
         Button buttonContinue = new Button();
@@ -72,14 +59,31 @@ public class ConfigScene {
                 }
 
                 // Show next scene
-                // Main.getStage().setScene(//farmui);
+                // Main.getStage().setScene(//FarmUI);
             }
         });
 
-        // Stage, Scene, and Root
+        // Set Grid
+        GridPane grid = new GridPane();
+        grid.add(nameLabel, 0, 0);
+        grid.add(nameTextField, 1, 0);
+        grid.add(diffLabel, 0, 1);
+        grid.add(diffComboBox, 1, 1);
+        grid.add(seedLabel, 0, 2);
+        grid.add(seedComboBox, 1, 2);
+        grid.add(seasonLabel, 0, 3);
+        grid.add(seasonComboBox, 1, 3);
+
+        grid.setAlignment(Pos.CENTER);
+
+        // Scene and Root
         // Set Root
+        BackgroundFill background_fill = new BackgroundFill(Color.LIGHTBLUE, CornerRadii.EMPTY, Insets.EMPTY);
         VBox root = new VBox();
-        root.getChildren().addAll(nameHBox, diffHBox, seedHBox, seasonHBox, buttonContinue);
+        root.setSpacing(20);
+        root.setAlignment(Pos.CENTER);
+        root.setBackground(new Background(background_fill));
+        root.getChildren().addAll(grid, buttonContinue);
 
         // Set Scene
         return new Scene(root);
