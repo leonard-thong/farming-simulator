965da00d78d08d9db6e6954f5460e2d0a46c81ed
Leonard <thongyixuen@gmail.com>
2020-10-04 19:28:25 -0400

    Add implmentation for Inventory and Market controller

--- src/scenes/InventoryController.java
+++ src/scenes/InventoryController.java
@@ -1,4 +1,38 @@
 package scenes;
 
-public class InventoryController {
+import gameobjects.items.Item;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import main.Main;
+
+public class InventoryController extends Application {
+    Item[] inventory = Main.getPlayer().getInventory();
+
+    @Override
+    public void start(Stage inventoryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/scenes/Inventory.fxml"));
+
+        Scene scene = new Scene(root);
+        Stage stage = inventoryStage;
+        stage.setTitle("Inventory");
+        stage.setScene(scene);
+
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+        stage.setX(bounds.getMinX());
+        stage.setY(bounds.getMinY());
+        stage.setWidth(bounds.getWidth());
+        stage.setHeight(bounds.getHeight());
+
+        stage.show();
+
+        for (Item i : inventory) {
+
+        }
+    }
 }
--- src/scenes/MarketController.java
+++ src/scenes/MarketController.java
@@ -1,4 +1,34 @@
 package scenes;
 
-public class MarketController {
+import gameobjects.items.Item;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import main.Main;
+
+public class MarketController extends Application {
+    Item[] market = new Item[25];
+
+    @Override
+    public void start(Stage marketStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/scenes/Market.fxml"));
+
+        Scene scene = new Scene(root);
+        Stage stage = marketStage;
+        stage.setTitle("Market");
+        stage.setScene(scene);
+
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+        stage.setX(bounds.getMinX());
+        stage.setY(bounds.getMinY());
+        stage.setWidth(bounds.getWidth());
+        stage.setHeight(bounds.getHeight());
+
+        stage.show();
+    }
 }
