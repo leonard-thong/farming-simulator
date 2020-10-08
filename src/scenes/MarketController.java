4a31d3211d90f57e8c04a0ccc737cf7a0dd054ad
jtandon6 <tandonjayant@gatech.edu>
2020-10-09 01:55:53 +0300

    Fix market UI

--- src/scenes/MarketController.java
+++ src/scenes/MarketController.java
@@ -1,34 +1,104 @@
 package scenes;
 
 import gameobjects.items.Item;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Screen;
-import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import main.Main;
 
-public class MarketController extends Application {
-    Item[] market = new Item[25];
 
-    @Override
-    public void start(Stage marketStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/scenes/Market.fxml"));
+public class MarketController {
 
-        Scene scene = new Scene(root);
-        Stage stage = marketStage;
-        stage.setTitle("Market");
-        stage.setScene(scene);
+    @FXML
+    private ImageView marketImageView0;
+    @FXML
+    private ImageView marketImageView1;
+    @FXML
+    private ImageView marketImageView2;
+    @FXML
+    private ImageView marketImageView3;
+    @FXML
+    private ImageView marketImageView4;
+    @FXML
+    private ImageView marketImageView5;
+    @FXML
+    private ImageView marketImageView6;
+    @FXML
+    private ImageView marketImageView7;
+    @FXML
+    private ImageView marketImageView8;
+    @FXML
+    private ImageView marketImageView9;
+    @FXML
+    private ImageView marketImageView10;
+    @FXML
+    private ImageView marketImageView11;
+    @FXML
+    private ImageView marketImageView12;
+    @FXML
+    private ImageView marketImageView13;
+    @FXML
+    private ImageView marketImageView14;
+    @FXML
+    private ImageView marketImageView15;
+    @FXML
+    private ImageView marketImageView16;
+    @FXML
+    private ImageView marketImageView17;
+    @FXML
+    private ImageView marketImageView18;
+    @FXML
+    private ImageView marketImageView19;
+    @FXML
+    private ImageView marketImageView20;
+    @FXML
+    private ImageView marketImageView21;
+    @FXML
+    private ImageView marketImageView22;
+    @FXML
+    private ImageView marketImageView23;
+    @FXML
+    private ImageView marketImageView24;
 
-        Screen screen = Screen.getPrimary();
-        Rectangle2D bounds = screen.getVisualBounds();
-        stage.setX(bounds.getMinX());
-        stage.setY(bounds.getMinY());
-        stage.setWidth(bounds.getWidth());
-        stage.setHeight(bounds.getHeight());
+    @FXML
+    private Button dropButton;
 
-        stage.show();
+    private Item[] market = new Item[25];
+
+    @FXML
+    void buy() {
+    }
+
+    @FXML
+    void sell(){
+
+    }
+
+    public void setImages() {
+        marketImageView0.setImage(market[0].getImage());
+        marketImageView1.setImage(market[1].getImage());
+        marketImageView2.setImage(market[2].getImage());
+        marketImageView3.setImage(market[3].getImage());
+        marketImageView4.setImage(market[4].getImage());
+        marketImageView5.setImage(market[5].getImage());
+        marketImageView6.setImage(market[6].getImage());
+        marketImageView7.setImage(market[7].getImage());
+        marketImageView8.setImage(market[8].getImage());
+        marketImageView9.setImage(market[9].getImage());
+        marketImageView10.setImage(market[10].getImage());
+        marketImageView11.setImage(market[11].getImage());
+        marketImageView12.setImage(market[12].getImage());
+        marketImageView13.setImage(market[13].getImage());
+        marketImageView14.setImage(market[14].getImage());
+        marketImageView15.setImage(market[15].getImage());
+        marketImageView16.setImage(market[16].getImage());
+        marketImageView17.setImage(market[17].getImage());
+        marketImageView18.setImage(market[18].getImage());
+        marketImageView19.setImage(market[19].getImage());
+        marketImageView20.setImage(market[20].getImage());
+        marketImageView21.setImage(market[21].getImage());
+        marketImageView22.setImage(market[22].getImage());
+        marketImageView23.setImage(market[23].getImage());
+        marketImageView24.setImage(market[24].getImage());
     }
 }
